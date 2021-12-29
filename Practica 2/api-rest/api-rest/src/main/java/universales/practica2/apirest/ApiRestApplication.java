3124dcd48372c38cddd4e530384810238df7acf6
Carlos <carlosorantes77@gmailcom>
2021-12-29 15:54:17 -0600

    Proyecto 2

---
+++ Practica 2/api-rest/api-rest/src/main/java/universales/practica2/apirest/ApiRestApplication.java
@@ -0,0 +1,13 @@
+package universales.practica2.apirest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRestApplication.class, args);
+	}
+
+}
