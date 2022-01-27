320fa6964f7db544592f03468687a2baf367c363
Carlos <carlosorantes77@gmailcom>
2022-01-27 17:43:31 -0600

    Done

---
+++ Practica 2/api-rest/src/test/java/universales/proyecto2/apirest/ProcedimientoServiceImplTest.java
@@ -0,0 +1,49 @@
+package universales.proyecto2.apirest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import java.util.Date;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import universales.library.dto.practica2.FiltrarPolizaDto;
+import universales.library.dto.practica2.SegurosDto;
+import universales.proyecto2.apirest.service.ProcedimientoService;
+
+@SpringBootTest
+class ProcedimientoServiceImplTest {
+	
+	@Autowired 
+	ProcedimientoService procedimientoService; 
+	
+	@Test
+	void insertPoliza() {
+		
+		SegurosDto segurosDto =  new SegurosDto();
+		segurosDto.setNumeroPoliza(0);
+		segurosDto.setRamo("Seguro de vida");
+		segurosDto.setFechaInicio(new Date());
+		segurosDto.setFechaVencimiento(new Date());
+		segurosDto.setCondicionesParticulares("");
+		segurosDto.setObservaciones("Nuevo seguiro adquirido");
+		segurosDto.setClienteDniCl(1);
+		
+		int res =  this.procedimientoService.insertarPoliza(segurosDto);
+		assertEquals(1, res);
+	}
+	
+	@Test
+	void filtrarPoliza() {
+		
+		SegurosDto segurosDto =  new SegurosDto();
+		segurosDto.setFechaInicio(new Date());
+		segurosDto.setClienteDniCl(1);
+		FiltrarPolizaDto polizaDto = this.procedimientoService.filtrarPoliza(segurosDto);
+		
+		assertNotNull(polizaDto);
+	}
+
+}
